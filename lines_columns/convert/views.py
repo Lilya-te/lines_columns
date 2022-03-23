bd3aa32578ce9f12c03ef626a52bbcd787699a96
Liliia <k.9602585349@gmail.com>
2022-03-24 00:12:45 +0300

    convert urls templates

--- lines_columns/convert/views.py
+++ lines_columns/convert/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def column_to_string(det):
+    return None
+
+def string_to_column(det):
+    return None
+
+def add_to_lines(side, string):
+    return None
+
+def delete_from_lines(side, count):
+    return None
+
+def set_column():
+    return None
+
+def parse_column_to_array():
+    return None
